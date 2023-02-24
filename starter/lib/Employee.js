3c19456cdf838a79958241c7765ed48adb669482
Darsing <catalunasiegfried@gmail.com>
2023-02-24 19:24:05 +0000

    arrange file set up

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+//inital code provided during office hour class by Dan(instructor)
+class Employee {
+    constructor(name, id, email) { 
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+
+    getName() {
+
+    };
+
+    getId() {
+
+    };
+
+    getEmail() {
+
+    };
+
+    getRole() {
+        return Employee;
+    };
+}
+
+module.exports = Employee;
--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+//initial provided by Dan(instructor)
+
+const Employee = require("../lib/Employee");
+
+
+class Intern extends Employee {
+    constructor(name, id, email, school) { 
+        super(name, id, email);
+        this.school = school;
+        
+    }
+
+    getSchool() {
+
+    };
+
+    getRole() {
+        return Intern;
+    }
+}
+
+module.exports = Intern;
